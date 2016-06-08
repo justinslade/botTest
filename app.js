e45c9f64a77b51c57c7b7a18d124020c0024d7dc
Justin Slade <slade.justin@gmail.com>
2016-06-08 14:19:48 +0200

    fixes bug

--- app.js
+++ app.js
@@ -17,13 +17,14 @@
 
 //require('@google/cloud-debug');
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
 // parse application/x-www-form-urlencoded
-//app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-//app.use(bodyParser.json());
+app.use(bodyParser.json());
 app.get('/', function (req, res) {
   res.status(200).send('Hello, world!');
 });
